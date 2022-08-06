b9fea572534dd225305237ffe2fe4988ebb57784
MuratGider35 <m.gider35@gmail.com>
2022-08-06 15:15:52 +0300

    jssession1

---
+++ app.js
@@ -0,0 +1,15 @@
+// console.log("app.js is loaded")
+// let firstname =prompt('Enter your name:')
+// alert('firstname :'+ firstnamemurat)
+// let sonuc = confirm('are you legal');
+// console.log(sonuc);
+var myNumber1 = 11;
+console.log(myNumber1);
+/*
+
+
+
+some function
+*/
+var myNumber1 = 22;
+console.log(myNumber1);
