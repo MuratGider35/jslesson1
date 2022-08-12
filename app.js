28015918ee55c108e31a3fcb65e7ceff99a19758
MuratGider35 <m.gider35@gmail.com>
2022-08-12 13:53:49 +0300

    added new js

--- app.js
+++ app.js
@@ -3,13 +3,50 @@
 // alert('firstname :'+ firstnamemurat)
 // let sonuc = confirm('are you legal');
 // console.log(sonuc);
-var myNumber1 = 11;
-console.log(myNumber1);
+// var myNumber1 = 11;
+// console.log(myNumber1);
 /*
 
 
 
 some function
 */
-var myNumber1 = 22;
-console.log(myNumber1);
+// var myNumber1 = 22;
+// console.log(myNumber1);
+// console.log('A')
+// var a = 5;
+// console.log(a);
+
+// console.log(typeof 0)
+// console.log(typeof "murat")
+// console.log(typeof true)
+// console.log(typeof '3>2')
+// let b = prompt('Bir sayı giriniz')
+// console.log(typeof b)
+
+// let x = 3;
+// console.log (x==3);
+
+// let username = "murat";
+// console.log(`username : ${username}`);
+
+
+// let a = +prompt("1.sayı gir");
+// let b = +prompt("2.sayı gir");
+// console.log(a*b);
+// console.log(a/b);
+
+// let kenar1 = +prompt("1.kenar");
+// let kenar2 = +prompt("2.kenar");
+// console.log(`diktörtgenin alanı: ${(kenar1*kenar2)/2}`);
+
+// let cdeğer = +prompt("celcius değeri gir");
+// console.log(`fahrenheit değeri: ${(cdeğer*9+(32*5))/5}`);
+
+// const today = new Date();
+// console.log(today);
+// console.log(today.getMonth(today));
+let a = +prompt("1.sayıyı giriniz");
+let b = +prompt("2.sayıyı giriniz");
+let b = +prompt("3.sayıyı giriniz");
+const sonuç = (a%10===b%10 && a%10==c%10
